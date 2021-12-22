68f76f9540d1558dcc532ea80974a60380700bc1
Avanya Haldar <82445699+AvanyaHaldar@users.noreply.github.com>
2021-12-22 20:04:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+var gender="";
+
 function preload() {
+lipstick=loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
+mustache=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 
 }
 
@@ -14,20 +18,35 @@ function setup() {
 
 function modelLoaded() {
   console.log("PoseNet is Initialized");
+  
 }
 
 function draw() {
   image(video, 0, 0, 500, 400);
+  gender=document.getElementById("gender").value;
+  console.log("Gender = "+ gender);
+  if (gender=="girl") {
+    image(lipstick,noseX-25,noseY+14,55,30);
+  }
+  else{
+    image(mustache,noseX-28,noseY+5,60,30);
+  }
+  
 }
 
 function takeSnapshot() {
   save("My_Filtered_Image.png");
 }
 
+var noseX = "";
+var noseY = "";
+
 function getPoses(results) {
   if (results.length > 0) {
     console.log(results);
-    console.log("nose x=" + results[0].pose.nose.x);
-    console.log("nose y=" + results[0].pose.nose.y);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("nose x=" + noseX);
+    console.log("nose y=" + noseY);
   }
 }
